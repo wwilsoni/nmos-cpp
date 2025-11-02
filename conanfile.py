614f2920192cc6ec5b86a044446a790687a67e21
wwilsoni <walterwilson0504@gmail.com>
2025-11-01 22:30:41 -0700

    Update: package-info

--- conanfile.py
+++ conanfile.py
@@ -36,14 +36,14 @@ class NmosCppConan(ConanFile):
         cmake.install()
 
     def package_info(self):
-        # Main target
+        # Export a proper CMake package
         self.cpp_info.set_property("cmake_file_name", "nmos-cpp")
         self.cpp_info.set_property("cmake_target_name", "nmos-cpp::nmos-cpp")
 
-        # Libraries to link (adjust if your CMake installs a different name)
+        # Libraries to link (adjust if your CMake actually produces a different name)
         self.cpp_info.libs = ["nmos-cpp"]
 
-        # Propagate dependencies
+        # Propagate dependencies downstream
         self.cpp_info.requires = [
             "boost::boost",
             "cpprestsdk::cpprest",
@@ -52,3 +52,4 @@ class NmosCppConan(ConanFile):
             "nlohmann_json_schema_validator::nlohmann_json_schema_validator",
             "jwt-cpp::jwt-cpp"
         ]
+
